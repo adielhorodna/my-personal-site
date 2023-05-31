60e76ca8dc43b1b21420fc238532330daf8464a6
Adiel Eichenstein <adiel.eich@gmail.com>
2023-05-31 13:31:19 -0400

    progress on about page

--- src/About.js
+++ src/About.js
@@ -1,45 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-// import myImage from './Adiel-Headshot-DSC04502-Edit.png';
 
-function About () {
 
-    return (
-        <section class="about" id="about">
-            <img src = "/Adiel-Headshot-DSC04502-Edit.png" alt = "profile photo"></img>
-        <img
-          src="assets/images/wfh_1.svg"
-          alt="adiel-photo"
-          loading="lazy"
-          class="hero-img"
-        />
-        <div class="">
-          <h2 class="bio-title">About Me</h2>
-          <p class="bio-text">
-            As a recent graduate in Full Stack Software Engineering,
-             I possess natural leadership skills coupled with a profound 
-             love for teamwork, organization, and planning. My passion for
-              collaboration, coupled with a strong work ethic, allows me to
-               excel in a team-oriented environment while contributing to the 
-               overall success of the organization.
-  
-  I have a proven track record of being a great team member, consistently 
-  bringing a positive attitude, and being committed to achieving shared 
-  objectives. My ability to communicate effectively with individuals at 
-  all levels enables me to build lasting relationships and facilitate
-  productive teamwork.  
-  
-  I am currently seeking an opportunity to leverage my skills and 
-  expertise in a challenging and dynamic environment. With a strong desire
-   to contribute my talents and knowledge, I am confident in my ability 
-   to make a significant impact and help drive success within the 
-   organization.
-          </p>
-        </div>
-  </section>
-    )
+
+import { Box, Text, Image, VStack } from '@chakra-ui/react';
+
+function About() {
+  return (
+    <Box id="about" textAlign="center" maxW="500px" mx="auto">
+      <Text as="h1" fontWeight="bold" fontSize="8xl" textAlign="left" mb={4}>
+        HI
+      </Text>
+      <Text textAlign="left" mb={4}>
+        My name is Adiel Eichenstein. I am a Full Stack Software Engineer and Web
+        Developer. I possess natural leadership skills coupled with a profound
+        love for teamwork, organization, and planning.
+      </Text>
+      <Text textAlign="left" mb={4}>
+        I have experience with a range of technologies, including HTML, CSS,
+        JavaScript, React, Ruby, and Ruby on Rails. My passion for collaboration,
+        coupled with a strong work ethic, allows me to excel in team-oriented
+        environments.
+      </Text>
+      <Text textAlign="left" mb={4}>
+        If you're seeking a dedicated and skilled professional to join your team or
+        tackle a freelance project, I would love to hear from you.
+      </Text>
+      <Text textAlign= "left" mb={8}>Let's chat.</Text>
+
+
+      <Image
+        src="https://raw.githubusercontent.com/adielhorodna/my-personal-site/main/Adiel-Headshot-DSC04502-Edit.png"
+        alt="profile photo"
+        boxSize="300px"
+        mb={4}
+        m1="auto"
+      />
+      </Box>
+  );
 }
-    export default About
+
+export default About;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function About () {
+
+//     return (
+//         <section className="about" id="about">
+//             <img src = "https://raw.githubusercontent.com/adielhorodna/my-personal-site/main/Adiel-Headshot-DSC04502-Edit.png" alt = "profile photo"/>
+
+
+//             <p> HI</p>
+//             <p> My name is Adiel Eichenstein. 
+//             I am a  Full Stack Software Engineer and Web Developer.
+//              I possess natural leadership skills coupled with a profound 
+//              love for teamwork, organization, and planning. </p>
+
+//              <p>I have experience using a  range of technologies, 
+//                including HTML, CSS, JavaScript, React, Ruby, and Ruby on Rails.
+//                My passion for collaboration, coupled with a strong work ethic, allows me to
+//                excel in team-oriented environments. </p>
+
+//  <p> If you're seeking a dedicated and skilled professional to join
+//   your team or tackle a freelance project, I would love to hear from you. </p>
+
+//   <p> Let's chat.</p>
+
+//   </section>
+
+//     )
+// }
+//     export default About
 
    
